eb13efb3e46a09dcdf40af3b3ceb0ac968cc6995
Lewis <lewis.horsley@hotmail.com>
2025-02-19 18:56:52 +0200

    fix colorscheme

--- init.lua
+++ init.lua
@@ -703,12 +703,6 @@ require('lazy').setup({
     'rose-pine/neovim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     config = function()
-      ---@diagnostic disable-next-line: missing-fields
-      require('tokyonight').setup {
-        styles = {
-          comments = { italic = false }, -- Disable italics in comments
-        },
-      }
       vim.cmd.colorscheme 'rose-pine'
     end,
   },
